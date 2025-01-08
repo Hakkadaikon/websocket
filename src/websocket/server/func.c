2c9c9b225ef7165c736781d105a69aae1e2a45bf
hakkadaikon <hakkadaikon@yahoo.co.jp>
2025-01-08 10:52:06 +0900

    Add error dump (send)

--- src/websocket/server/func.c
+++ src/websocket/server/func.c
@@ -1,4 +1,5 @@
 #include <arpa/inet.h>
+#include <errno.h>
 #include <stdint.h>
 #include <string.h>
 #include <unistd.h>
@@ -15,5 +16,15 @@ int websocket_server_close(const int server_sock)
 int websocket_server_send(const int client_sock, const char* restrict buffer, const size_t buffer_size)
 {
     log_debug("WebSocket server send\n");
-    return send(client_sock, buffer, buffer_size, 0);
+
+    int rtn = send(client_sock, buffer, buffer_size, 0);
+    if (rtn == -1) {
+        char* reason = strerror(errno);
+        log_error("Failed to send().\n");
+        log_error("reason : ");
+        log_error(reason);
+        log_error("\n");
+    }
+
+    return rtn;
 }
