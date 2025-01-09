8e93fa431f1051c2d70e5514518d1d46fb678498
hakkadaikon <hakkadaikon@yahoo.co.jp>
2025-01-09 09:05:53 +0900

    Add test

---
+++ tests/crypto/base64.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+#include "../../src/crypto/base64.h"
+
+TEST(Base64Test, Base64NormalTest) {
+    //base64_encode(uint8_t *input, size_t input_len, char *output, const size_t output_len)
+    int a;
+}
