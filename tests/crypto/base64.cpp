9ae47c3c39709041127e62547eaf972d40565c96
hakkadaikon <hakkadaikon@yahoo.co.jp>
2025-01-11 03:38:09 +0900

    Update googletest install method.

--- tests/crypto/base64.cpp
+++ tests/crypto/base64.cpp
@@ -1,7 +1,9 @@
-#include <gtest/gtest.h>
 #include "../../src/crypto/base64.h"
 
-TEST(Base64Test, Base64NormalTest) {
+#include <gtest/gtest.h>
+
+TEST(Base64Test, Base64NormalTest)
+{
     //base64_encode(uint8_t *input, size_t input_len, char *output, const size_t output_len)
     int a;
 }
