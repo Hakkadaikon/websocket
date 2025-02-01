3717363bf71729804f77ea6580b379ea8bfdcd4c
hakkadaikon <hakkadaikon@yahoo.co.jp>
2025-02-02 05:09:51 +0900

    Use standard bsd socket api. Changed it to calling the syscall directly, but the speed didn't change much.

--- src/websocket/socket/accept.c
+++ src/websocket/socket/accept.c
@@ -5,12 +5,6 @@
 #include "../websocket_local.h"
 #include "optimize_socket.h"
 
-#ifdef __APPLE__
-#define SYSCALL_ACCEPT(fd, addr, addr_len) accept(fd, addr, addr_len)
-#else
-#define SYSCALL_ACCEPT(fd, addr, addr_len) syscall(SYS_accept, fd, addr, addr_len)
-#endif
-
 int websocket_accept(const int sock_fd)
 {
     struct sockaddr_in client_addr;
@@ -22,7 +16,7 @@ int websocket_accept(const int sock_fd)
     }
 
     log_debug("accept...\n");
-    int client_sock = SYSCALL_ACCEPT(sock_fd, (struct sockaddr*)&client_addr, &addr_len);
+    int client_sock = accept(sock_fd, (struct sockaddr*)&client_addr, &addr_len);
     if (client_sock < 0) {
         if (errno == EINTR || errno == EAGAIN) {
             return WEBSOCKET_ERRORCODE_CONTINUABLE_ERROR;
