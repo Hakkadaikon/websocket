3dbd6276b4d807826beed302f8faf3fef7814253
hakkadaikon <hakkadaikon@yahoo.co.jp>
2025-02-03 02:27:20 +0900

    Fix resource leak to websocket_accept()

--- src/websocket/socket/accept.c
+++ src/websocket/socket/accept.c
@@ -27,6 +27,7 @@ int websocket_accept(const int sock_fd)
     }
 
     if (optimize_client_socket(client_sock) == WEBSOCKET_ERRORCODE_FATAL_ERROR) {
+        websocket_close(client_sock);
         return WEBSOCKET_ERRORCODE_CONTINUABLE_ERROR;
     }
 
