e95349517515bb0b852fd75aca1a580670c21f30
hakkadaikon <hakkadaikon@yahoo.co.jp>
2025-02-11 15:08:58 +0900

    Fix run.sh

--- shell/run.sh
+++ shell/run.sh
@@ -2,17 +2,37 @@
 
 cd $(dirname $0)
 
+ECHOBACK_SERVER_PATH=../examples/echoback
+ECHOBACK_SERVER_BIN_PATH=${ECHOBACK_SERVER_PATH}/bin/wsserver
+
+wsDebugBuild() {
+  cmake -S .. -B ../build -DCMAKE_BUILD_TYPE=Debug 1>/dev/null
+  cmake --build ../build 1>/dev/null
+  make clean -C ${ECHOBACK_SERVER_PATH} 1>/dev/null
+  BUILD=debug make -C ${ECHOBACK_SERVER_PATH} 1>/dev/null
+}
+
+wsReleaseBuild() {
+  cmake -S .. -B ../build -DCMAKE_BUILD_TYPE=Release 1>/dev/null
+  cmake --build ../build 1>/dev/null
+  make clean -C ${ECHOBACK_SERVER_PATH} 1>/dev/null
+  BUILD=release make -C ${ECHOBACK_SERVER_PATH} 1>/dev/null
+}
+
 case "$1" in
-    memcheck)
-        make debug-build -C ..
-        valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all --track-origins=yes -s ../result/bin/ws-server
-        ;;
-    helgrind)
-        make debug-build -C ..
-        valgrind --tool=helgrind --history-level=approx -s ../result/bin/ws-server
-        ;;
-    *)
-        cd .. && nix-build
-        result/bin/ws-server
-        ;;
+  memcheck)
+    wsDebugBuild
+    echo "debug run (memcheck)"
+    valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all --track-origins=yes -s ${ECHOBACK_SERVER_BIN_PATH}
+    ;;
+  helgrind)
+    wsDebugBuild
+    echo "debug run (helgrind)"
+    valgrind --tool=helgrind --history-level=approx -s ${ECHOBACK_SERVER_BIN_PATH}
+    ;;
+  *)
+    wsReleaseBuild
+    echo "release run"
+    ${ECHOBACK_SERVER_BIN_PATH}
+    ;;
 esac
