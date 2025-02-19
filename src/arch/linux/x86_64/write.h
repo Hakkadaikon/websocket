ee3646fa9b589d10c1889ad33c3ca19ef94601d0
hakkadaikon <hakkadaikon@yahoo.co.jp>
2025-02-19 12:22:36 +0900

    Change to call syscall from assembly

--- src/arch/linux/x86_64/write.h
+++ src/arch/linux/x86_64/write.h
@@ -7,15 +7,12 @@
 
 static inline ssize_t linux_x8664_write(const int fd, const void* buf, const size_t count)
 {
-    long ret;
-    __asm__ volatile(
-        "syscall"
-        : "=a"(ret)
-        : "0"(__NR_write),
-          "D"(fd),
-          "S"(buf),
-          "d"(count)
-        : "rcx", "r11", "memory");
+    long ret = linux_x8664_asm_syscall3(
+        __NR_write,
+        fd,
+        buf,
+        count);
+
     if ((unsigned long)ret >= (unsigned long)-4095) {
         errno = -ret;
         ret   = -1;
